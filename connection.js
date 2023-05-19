de7273bb9c88204df42ccbf78ec3172b10dcebde
batwood99 <atwood@mail.usf.edu>
2023-05-19 14:12:38 -0400

    corrected database name to employees

--- connection.js
+++ connection.js
@@ -4,7 +4,7 @@ const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "password",
-  database: "Employee_Tracker"
+  database: "employees"
 });
 
 connection.connect(function (err) {
--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'password',
-  database: 'Employee_Tracker'
+  database: 'employees'
 }, console.log('Connected to the election database.'));
 
 db.connect();
