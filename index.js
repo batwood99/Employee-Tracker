c0adee9ca32d223cab92770ad4e1c3eabaae8bde
batwood99 <atwood@mail.usf.edu>
2023-05-19 10:55:36 -0400

    added add role and add department

--- index.js
+++ index.js
@@ -56,3 +56,74 @@ function chooseRole() {
     }
   });
 }
+
+function viewDepartments() {
+  db.query(`SELECT * FROM department;`, (err, res) => {
+    console.table(res);
+    chooseRole();
+  });
+}
+
+function viewRoles() {
+  db.query(`SELECT * FROM role;`, (err, res) => {
+    if (err) {
+      console.log(err);
+    }
+    console.table(res);
+    chooseRole();
+  });
+}
+
+function viewEmployees() {
+  db.query(`SELECT * FROM employee;`, (err, res) => {
+    if (err) {
+      console.log(err);
+    }
+    console.table(res);
+    chooseRole();
+  });
+}
+
+function addDepartment() {
+  inquirer.prompt({
+    name: "Department_name",
+    message: "What's the name of the department?",
+    type: "input"
+  }).then(ans => {
+    db.query(`INSERT INTO department(name) VALUES("${ans.Department_name}");`, (err, res) => {
+      if (err) {
+        console.log(err);
+      }
+      console.table(res);
+      chooseRole();
+    });
+  });
+}
+
+function addRole() {
+  inquirer.prompt([
+    {
+      name: "Role_name",
+      message: "What's the name of the role you'd like to add?",
+      type: "input"
+    },
+    {
+      name: "Salary",
+      message: "What's the salary for this role?",
+      type: "input"
+    },
+    {
+      name: "Role_Department",
+      message: "What department ID is this role in?",
+      type: "input"
+    }
+  ]).then(roleAns => {
+    db.query(`INSERT INTO role(title, salary, department_id) VALUES("${roleAns.Role_name}", ${roleAns.Salary}, ${roleAns.Role_Department});`, (err, res) => {
+      if (err) {
+        console.log(err);
+      }
+      console.table(res);
+      chooseRole();
+    });
+  });
+}
